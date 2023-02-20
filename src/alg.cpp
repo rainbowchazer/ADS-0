b6d0c5db1fce304acfe19a1a9920e7ee7ae31c18
rainbowchazer <125148013+rainbowchazer@users.noreply.github.com>
2023-02-20 22:21:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a >= b)
+    {
+        for (int i = b; i > 0; i--)
+        {
+            if ((b % i) == 0 & (a % i) == 0)
+                return i;
+        }
+    }
+    else {
+        for (int i = a; i > 0; i--)
+        {
+            if ((b % i) == 0 & (a % i) == 0)
+                return i;
+        }
+    }
 }
