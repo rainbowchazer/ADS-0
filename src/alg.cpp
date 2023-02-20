f9b6795e86670c64bbe12c1df7fc15e9b0b07c75
rainbowchazer <125148013+rainbowchazer@users.noreply.github.com>
2023-02-20 23:16:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if (a >= b) {
-        for (int i = b; i > 0; i--) {
-            if ((b % i) == 0 && (a % i) == 0)
-                return i;
-        }
-    }
-    else {
-        for (int i = a; i > 0; i--) {
-            if ((b % i) == 0 && (a % i) == 0)
-                return i;
-        }
+    int min;
+    if (a >= b)
+        min = b;
+    else 
+        min = a;
+    for (int i = min; i > 0; i--) {
+        if ((b % i) == 0 && (a % i) == 0)
+            return i;
     }
 }
